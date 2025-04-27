9138e664aad981de017514d627cb333cf7bf1148
Daniel Skowroński <daniel@skowron.ski>
2025-04-27 03:57:11 +0200

    migrate to HomeSpan, implement basic RGB+W light and Night Light
    - migrate to pioarduino to get Arduino Core 3
    - use HomeSpan->Pixel instead of NeoPixelBus (extra benefit of working
    on Arduino Core 3, which is required by HomeSpan 2)
    - implement pair of HomeKit lights: RGB and W-only variants to get fine
    control over light from outside automations
    - add separate task to trigger night light (check HomeKit target and ambient
    light values)

--- README.md
+++ README.md
@@ -57,3 +57,38 @@ Later, some extra sensors may be added, preferably using the I2C bus to keep the
 For external connections, I chose screwable connectors: USB-C socket for panel mounting and cable gland for the rest of wires (3 thicker for LED, 4 thin for I2C).
 
 I opted for a ready-made enclosure (Kradex Z58JH PS) instead of custom 3D-printed to avoid the hassle of printing holes for external connectors and to save time (even MVP requires an enclosure due to hanging wires and high current).
+
+---
+
+## Licences
+
+### Example from HomeSpan
+
+```c
+/*********************************************************************************
+ *  MIT License
+ *
+ *  Copyright (c) 2022 Gregg E. Berman
+ *
+ *  https://github.com/HomeSpan/HomeSpan
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *
+ ********************************************************************************/
+ ```
--- software/DoorLedStrip/src/main.cpp
+++ software/DoorLedStrip/src/main.cpp
@@ -1,76 +1,94 @@
-#include <Arduino.h>
-
-#define LED_COUNT 60
-#include <NeoPixelBus.h>
-NeoPixelBus<NeoGrbwFeature, NeoEsp32I2s1X8Sk6812Method> led(LED_COUNT, 16); 
-
+#include "HomeSpan.h"
 #include <Wire.h>
 #include <BH1750.h>
-#include <Adafruit_INA219.h>
 
-Adafruit_INA219 ina219;
+#define MAX_CURRENT 2.0
+TaskHandle_t CurrentMonitorTaskHandle; // TODO
+
+#define NPIXELS 60
+Pixel pixel(16, "GRBW");
+Pixel::Color colorRGB; 
+Pixel::Color colorWC;
+
 BH1750 lightMeter(0x23);
 
-void print_sensors(){
-  Serial.printf("%2.2f V | %4d mA | %5d lx\n", ina219.getBusVoltage_V(), int(ina219.getCurrent_mA()), int(lightMeter.readLightLevel()));
+#define NIGHT_LIGHT_THRESHOLD 100
+#define NIGHT_LIGHT_BRIGHTNESS 32
+TaskHandle_t NightLightTaskHandle;
+bool areLightsOff(){
+  return (colorRGB==Pixel::RGB(0,0,0,0) && colorWC==Pixel::RGB(0,0,0,0));
 }
+void NightLightTask( void * parameter) {
+  for(;;) {
+    int lux = lightMeter.readLightLevel();
+    Serial.printf("Lux: %d\n", lux);
+    if (areLightsOff()) {
+      if (lux > NIGHT_LIGHT_THRESHOLD) {
+        pixel.set(Pixel::RGB(0, 0, 0, 0), NPIXELS);
+      } else {
+        pixel.set(Pixel::RGB(0, 0, 0, NIGHT_LIGHT_BRIGHTNESS), NPIXELS);
+      }
+    }
+    vTaskDelay(1000);
+  }
+}
+struct NeoPixel_RGB : Service::LightBulb { 
+  Characteristic::On power{0,true};
+  Characteristic::Hue H{0,true};
+  Characteristic::Saturation S{0,true};
+  Characteristic::Brightness V{100,true};
+  int nPixels;
+  
+  NeoPixel_RGB() : Service::LightBulb(){
+    V.setRange(1,100,1);
+    update();
+  }
+
+  boolean update() override {
+    colorRGB=pixel.HSV(H.getNewVal(), S.getNewVal(), V.getNewVal()*power.getNewVal());
+    pixel.set(colorRGB+colorWC,NPIXELS);
+    return(true);
+  }
+};
+struct NeoPixel_W : Service::LightBulb {
+  Characteristic::On power{0,true};
+  Characteristic::Brightness V{100,true};
+  
+  NeoPixel_W() : Service::LightBulb(){
+    V.setRange(1,100,1);
+    update();
+  }
+
+  boolean update() override {
+    colorWC=pixel.CCT(1.0e6/4000, V.getNewVal()*power.getNewVal());
+    pixel.set(colorRGB+colorWC,NPIXELS);
+    return(true);
+  }
+};
+
+// TODO: fork Pixel class to add setting each pixel to a different color; ideally make a PR to HomeSpan to get some fields as protected
 
 void setup() {
   Serial.begin(115200);
-  Serial.println();
-  Serial.println("Initializing...");
-  Serial.flush();
-
-  Wire.begin();
-
+  Wire.begin(21, 22);
   lightMeter.begin();
-  ina219.begin();
-  led.Begin();
-
-  Serial.println();
-  Serial.println("Running...");
-
-  Serial.println("Setting all RED");
-  for (int i=0; i<LED_COUNT; i++){
-    led.SetPixelColor(i, RgbwColor(255, 0, 0, 0));
-    led.Show();
-    delay(10);
-  }
-  print_sensors();
-  Serial.println("Setting all GREEN");
-  for (int i=0; i<LED_COUNT; i++){
-    led.SetPixelColor(i, RgbwColor(0, 255, 0, 0));
-    led.Show();
-    delay(10);
-  }
-  print_sensors();
-  Serial.println("Setting all BLUE");
-  for (int i=0; i<LED_COUNT; i++){
-    led.SetPixelColor(i, RgbwColor(0, 0, 255, 0));
-    led.Show();
-    delay(10);
-  }
-  print_sensors();
-  Serial.println("Setting all WHITE");
-  for (int i=0; i<LED_COUNT; i++){
-    led.SetPixelColor(i, RgbwColor(0, 0, 0, 255));
-    led.Show();
-    delay(10);
-  }
-  print_sensors();
-
-  Serial.println("In 3s, setting all RGB to RANDOM every 1s");
-  delay(3000);
+ 
+  homeSpan.begin(Category::Lighting,"DoorLedStrip","DoorLedStrip","v0.1");
+  SPAN_ACCESSORY();
+  SPAN_ACCESSORY("DoorLedStrip White");
+    new NeoPixel_W();
+  SPAN_ACCESSORY("DoorLedStrip RGB");
+    new NeoPixel_RGB();
+  
+  xTaskCreate(
+    NightLightTask,
+    "NightLightTask",
+    10000,
+    NULL,
+    1,
+    &NightLightTaskHandle);
+  delay(500); 
 }
-
 void loop() {
-  int r=random(256);
-  int g=random(256);
-  int b=random(256);
-  for (int i=0; i<LED_COUNT; i++){
-    led.SetPixelColor(i, RgbwColor(r,g,b, 0));
-  }
-  led.Show();
-  print_sensors();
-  delay(1000);
+  homeSpan.poll();
 }
